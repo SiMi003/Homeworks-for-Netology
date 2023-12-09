861a0990412f0d5b196f06a5aefa6fdbb5ba1c85
Aleksandra Sidorova <alexandra.a.sidorova@gmail.com>
2023-12-09 15:13:53 +0100

    homework, work with json and xml

---
+++ homework3_json/using_json.py
@@ -0,0 +1,47 @@
+﻿import json
+
+
+def read_json(file_path, word_max_len=6, top_words_amt=10):
+    """
+    функция для чтения файла с новостями.
+    """
+    # Ваш алгоритм
+    with open(file_path, 'r', encoding='utf-8') as f:
+        json_data = json.load(f)
+    list_ = []
+    list_draft = []
+    for item in json_data["rss"]["channel"]["items"]:
+        list_draft = item["description"].strip().split()
+        for el in list_draft:
+            if len(el) > word_max_len:
+                list_.append(el)
+        
+    from collections import Counter
+    dict_ = Counter(list_)
+    raiting = sorted(list(dict_.items()), 
+                     key=lambda el: int(el[1]), reverse = True)[:top_words_amt]
+    popular_words = []
+    for el in raiting:
+        popular_words.append(el[0])
+        
+    return popular_words
+            
+
+if __name__ == '__main__':
+    print(read_json('newsafr.json'))
+
+
+
+
+# import xml.etree.ElementTree as ET
+
+
+# def read_xml(file_path, word_max_len=6, top_words_amt=10):
+#     """
+#     функция для чтения файла с новостями.
+#     """
+#     # Ваш алгоритм
+
+
+# if __name__ == '__main__':
+#     print(read_xml('newsafr.xml'))
---
+++ homework3_xml/using_xml.py
@@ -0,0 +1,36 @@
+﻿import xml.etree.ElementTree as ET
+
+
+def read_xml(file_path, word_max_len=6, top_words_amt=10):
+    """
+    функция для чтения файла с новостями.
+    """
+    
+    # loading data from the file
+    parser = ET.XMLParser(encoding='utf-8')
+    tree = ET.parse(file_path, parser)
+    # getting the tree root
+    root = tree.getroot()
+    xml_items = root.findall('channel/item')
+    
+    list_ = []
+    list_draft = []
+    for item in xml_items:
+        list_draft = (item.find('description').text).strip().split()
+        for el in list_draft:
+            if len(el) > word_max_len:
+                list_.append(el)
+       
+    from collections import Counter
+    dict_ = Counter(list_)
+    raiting = sorted(list(dict_.items()), 
+                     key=lambda el: int(el[1]), reverse = True)[:top_words_amt]
+    popular_words = []
+    for el in raiting:
+        popular_words.append(el[0])
+        
+    return popular_words
+
+
+if __name__ == '__main__':
+    print(read_xml('newsafr.xml'))
